4d6206c73165e2bde9c34e0362437f072e2a4fde
Xin Huang <xinhuang@thoughtworks.com>
2013-03-14 09:24:37 +0800

    [xin] add unhandled exception catch.

--- GoMonitor/Program.cs
+++ GoMonitor/Program.cs
@@ -15,7 +15,19 @@ namespace GoMonitor
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+            Application.ThreadException += Application_ThreadException;
+            AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(CurrentDomain_UnhandledException);
             Application.Run(new Form1());
+        }
+
+        static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show(e.ExceptionObject.ToString());
+        }
+
+        static void Application_ThreadException(object sender, System.Threading.ThreadExceptionEventArgs e)
+        {
+            MessageBox.Show(e.Exception.Message + "\n" + e.Exception.StackTrace);
         }
     }
 }
